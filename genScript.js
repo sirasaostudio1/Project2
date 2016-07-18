9c0713b7c3571d9140001e39bdf4000330489b6f
sirasaostudio1 <sirasaostudio1@gmail.com>
2016-07-18 10:52:44 -0700

    Add files via upload

---
+++ genScript.js
@@ -0,0 +1,80 @@
+//---------------------------------------------------PEAK CALLING--------------------------------------------------
+
+function myFunction() {
+    document.getElementById("myImg").src = "draw/pc.png";
+    myFunctionA();
+}
+
+function myFunctionA() {
+    document.getElementById("div1").style.display = "inline";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+
+}
+
+//---------------------------------------------------DIFF BIND--------------------------------------------------------
+
+function myFunction1() {
+    document.getElementById("myImg").src = "draw/diffn.png";
+    myFunction1A();
+}
+
+function myFunction1A() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "inline";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+
+}
+
+//---------------------------------------------------MOTIF ANA--------------------------------------------------------
+
+function myFunction2() {
+    document.getElementById("myImg").src = "draw/motifa.png";
+    myFunction2A();
+}
+
+function myFunction2A() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "inline";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+
+}
+
+//---------------------------------------------------PEAK ANNO--------------------------------------------------------
+
+function myFunction3() {
+    document.getElementById("myImg").src = "draw/peaka.png";
+    myFunction3A();
+}
+
+function myFunction3A() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "inline";
+    document.getElementById("div5").style.display = "none";
+
+}
+
+//---------------------------------------------------GENE ONTO--------------------------------------------------------
+
+function myFunction4() {
+    document.getElementById("myImg").src = "draw/goa.png";
+    myFunction4A();
+}
+
+function myFunction4A() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "inline";
+
+}
